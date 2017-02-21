dfb6f9abd7a1029acc6105498a891708644297ad
MyteD <elvisyuan6988@gmail.com>
2017-02-21 17:45:42 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,64 +4,70 @@
 using namespace std;
 
 int time[100];
+
 int record[100];
+int countR = 0;
 
-
-bool ifGreen(int time,int length,int t[]) {
+bool ifGreen(int time, int length, int t[]) {
+	
 	for (int i = 0; i < length; i++) {
-		if ((time / t[i] + 1) % 2 == 0)
-			return false;
-		if (((time/t[i])%2==0 &&(time + 5) / t[i] + 1) % 2 == 0)
+		int temp = time % (2 * t[i]);
+		if (temp >= t[i] - 5)
 			return false;
 	}
 	return true;
+	
 }
 
-void countTime(int length,int t[]) {
+void countTime(int length, int t[]) {
 	int m = t[0];
 
 	for (int i = 1; i < length; i++) {
 		m = min(t[i - 1], t[i]);
 	}
-	
-	for (int i = m-5; i < 18000;i++) {
-		if (ifGreen(i, length, t)) {
-			cout << "ok" << i << endl;
+
+	for (int i = m-4; i < 18000; i++) {
+		if (ifGreen(i, length, t)==true) {
+			record[countR] = i;
+			countR++;
 			break;
 		}
-		
-	
 	}
-	cout << "fail" << endl;
 	
+
 }
 
 int main() {
-	int temp=0;// count for total lights
-	int count0 = 0; 
+	int temp = 0;// count for total lights
+	int count0 = 0;
 	
+
 	while (true) {
 		for (int i = 0; i < 100; i++) {
 			cin >> time[i];
-			
-			if (time[i] == 0) {
+
+			if (time[i] == 0 ) {
 				count0++;
-				countTime(i,time);
-				cout << "time0"<<endl;
+				if(count0==1)
+					countTime(i, time);
+				cout << "time0" << endl;
+				
 				break;
 			}
-			
+			if(time[i]!=0)
+				count0 = 0;
+
 		}
-		if (count0 == 4){
+		if (count0 == 4) {
 			cout << "time0*4" << endl;
 			break;
 		}
-		else 
-			count0 = 0;
-		temp = 0;
-			
+		
 
 	}
-	
+
+	for (int i = 0; i < countR; i++) {
+		cout << record[i] << endl;
+	}
 	return 0;
 }
